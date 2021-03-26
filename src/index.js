a21a9193351a3bd218920cd6571c4c8846e8a2fa
Luis Felipe <luis.felipeszsv@gmail.com>
2021-03-25 22:03:33 -0300

    adicionando css e imagens

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
+const routes = require('./routes')
 
-app.get('/', (req, res) => {
-  return res.sendFile(__dirname + '/views/index.html')
-})
+app.use(express.static('public'))
 
 app.listen(3000, () => {
   console.log('rodando')
