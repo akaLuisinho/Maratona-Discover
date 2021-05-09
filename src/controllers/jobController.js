bcbf5fc45c1731d8d25f6b3ede39d85bdf8f2595
Luis Felipe <luis.felipeszsv@gmail.com>
2021-05-09 09:13:59 -0300

    refactoring profile controller

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -6,6 +6,7 @@ module.exports = {
     create(req, res) {
         return res.render('job')
     },
+
     async save(req, res) {
         const Jobs = await Job.get()
 
@@ -18,12 +19,14 @@ module.exports = {
 
         return res.redirect('/')
     },
+
     async show(req, res) {
         const profileData = await Profile.get()
         const Jobs = await Job.get()
 
         const jobId = req.params.id
         const job = Jobs.find(j => j.id == jobId)
+        
         job.budget = JobUtils.calculateBudget(job['total-hours'], profileData['value-hour'])
 
         if (!job) {
@@ -31,6 +34,7 @@ module.exports = {
         }
         return res.render('job-edit', { job })
     },
+
     async update(req, res) {
         const jobId = req.params.id
 
@@ -44,6 +48,7 @@ module.exports = {
 
         return res.redirect('/')
     },
+
     async delete(req, res) {
         const jobId = Number(req.params.id)
 
--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -5,23 +5,27 @@ module.exports = {
         const profileData = await Profile.get()
         return res.render('profile', { profile: profileData })
     },
+
     async update(req, res) {
      const profileData = await Profile.get()
         const data = req.body
 
+        //calculate weeks in vacation
         const weeksPerYear = 52
         const weeksPerMonth = (weeksPerYear - data['vacation-per-year']) / 12
 
-        //total de horas na semana
+        
         const weekTotalHours = data['hours-per-day'] * data['days-per-week']
         const monthlyTotalHours = weekTotalHours * weeksPerMonth
 
         const valueHour = data['monthly-budget'] / monthlyTotalHours
+        
         await Profile.update({
             ...profileData,
             ...req.body,
             'value-hour': valueHour
         })
+
         return res.redirect('profile')
     }
 }
