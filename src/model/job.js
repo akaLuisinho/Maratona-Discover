cccdafb457ae5f624c8fb344838c41cba5c612b9
Luis Felipe <luis.felipeszsv@gmail.com>
2021-05-09 09:17:48 -0300

    refactoring models

--- src/model/job.js
+++ src/model/job.js
@@ -2,10 +2,9 @@ const Database = require('../db/config')
 
 module.exports = {
     async get() {
+        const db = await Database()
 
-        db = await Database()
-
-        const data = await db.all(`SELECT * FROM jobs`)
+        const data = await db.all('SELECT * FROM jobs')
 
         await db.close()
 
@@ -17,39 +16,36 @@ module.exports = {
             created_at: job.created_at
         }))
     },
-    async update(updatedJob, id) {
-        db = await Database()
 
-        await db.run(`UPDATE jobs SET
+    async update(updatedJob, id) {
+        const db = await Database()
+
+        await db.run(`
+        UPDATE jobs SET
             name = "${updatedJob.name}",
             daily_hours = ${updatedJob['daily-hours']},
             total_hours = ${updatedJob['total-hours']}
-            WHERE id = ${id}`)
+        WHERE id = ${id}`
+        )
 
-        db.close()
+        await db.close()
     },
+
     async delete(id) {
-        db = await Database()
+        const db = await Database()
 
         await db.run(`DELETE FROM jobs WHERE id = ${id}`)
 
         await db.close()
     },
-    async create(newJob) {
-        db = await Database()
 
-        await db.run(`INSERT INTO jobs 
-        (
-        name,
-        daily_hours,
-        total_hours,
-        created_at
-        ) VALUES (
-            "${newJob.name}",
-            ${newJob['daily-hours']},
-            ${newJob['total-hours']},
-            ${newJob.created_at}
-        )`)
+    async create(newJob) {
+        const db = await Database()
+
+        await db.run(`
+        INSERT INTO jobs(name, daily_hours, total_hours, created_at) 
+        VALUES ("${newJob.name}", ${newJob['daily-hours']}, ${newJob['total-hours']}, ${newJob.created_at})
+        `)
 
         await db.close()
     }
--- src/model/profile.js
+++ src/model/profile.js
@@ -4,7 +4,7 @@ module.exports = {
     async get() {
         const db = await Database()
 
-        const data = await db.get(`SELECT * FROM profile`)
+        const data = await db.get('SELECT * FROM profile')
         
         await db.close() 
         
@@ -18,18 +18,21 @@ module.exports = {
             'value-hour': data.value_hour
         }
     },
+
     async update(newData) {
-       const db = await Database() 
+        const db = await Database() 
 
-       await db.run(`UPDATE profile SET 
-       name = "${newData.name}",
-       avatar = "${newData.avatar}",
-       monthly_budget = ${newData['monthly-budget']},
-       days_per_week = ${newData['days-per-week']},
-       hours_per_day = ${newData['hours-per-day']},
-       vacation_per_year = ${newData['vacation-per-year']},
-       value_hour = ${newData['value-hour']}`)
+        await db.run(`
+        UPDATE profile SET 
+            name = "${newData.name}",
+            avatar = "${newData.avatar}",
+            monthly_budget = ${newData['monthly-budget']},
+            days_per_week = ${newData['days-per-week']},
+            hours_per_day = ${newData['hours-per-day']},
+            vacation_per_year = ${newData['vacation-per-year']},
+            value_hour = ${newData['value-hour']}
+        `)
 
-       db.close()
+        db.close()
     }
 }
